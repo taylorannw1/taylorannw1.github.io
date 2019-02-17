327c849af98013375730df425344269ed035a58f
Taylor Annette <47403564+taylorannw1@users.noreply.github.com>
2019-02-17 11:13:39 -0500

    test

---
+++ control.js
@@ -0,0 +1,7 @@
+button onclick="myFunction()">Click me</button>
+button.onclick =function(){start};
+for(i=0;i<5;i++){
+ document.getElementById('deep').text.value = "Exhale..."
+ var timeoutID = scope.setTimeout(function[, 4000]);
+ document.getElementById('deep').value = "Exhale..."
+}
